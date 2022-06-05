2388b78cafd01133f1100b4f3e1a9d8bef9b7a32
Jack Wright <jack.louie.wright@gmail.com>
2022-06-05 13:26:28 +0100

    boilerplate for kick start

---
+++ 2013/boilerplate/cpp_boilerplate.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void read_input() {
+
+}
+
+void solve() {
+
+}
+
+int main() {
+    int ncases;
+    std::cin >> ncases;
+    for(int i=1; i<=ncases; i++) {
+        read_input();
+        solve();
+        std::cout << "Case #" << i << ": " << std::endl;
+    }
+}
