1f3d374f0f03d2197434a3ef177d935ed4571b97
Jack Wright <jack.louie.wright@gmail.com>
2022-06-07 23:18:22 +0100

    attempt to test for bipartiteness rather than connectedness

--- 2013/practice_round/task3.cpp
+++ 2013/practice_round/task3.cpp
@@ -7,10 +7,11 @@
 // split a group of 2M into two (not necessarily equally sized?) groups with 
 // no pairs in common.
 
-// is a graph connected (impossible), or disconnected (possible).
-// for an undirected graph, finding connected components with BFS/DFS is O(V+E)
+// if the graph is bipartite this is possible, if the graph is not bipartite this
+// is impossible. (bipartiteness can be detected in quadratic time.)
 
-// how to represent the graph? Every node needs to know adjacent nodes.
+// do a bfs in each connected component, coloring neighbouring vertices oppositely
+// to the current vertex.
 
 struct node {
     int node_idx;
@@ -20,16 +21,24 @@ struct node {
 class graph {
     private:
         std::vector<node> _nodes;
+        std::vector<bool> _cols; // colours of each node
         std::unordered_map<std::string, int> _idx_map;
         int _cur_idx;
-        void bfs(std::vector<bool>& visited, int idx) {
-            // do a bfs
+        bool bfs(std::vector<bool>& visited, int idx, bool col) {
             visited[idx] = true;
             for(int i: _nodes[idx].adj) {
                 if(!visited[i]) {
-                    bfs(visited, i);
+                    if(!bfs(visited, i, !col)) {
+                        return false;
+                    }
+                }
+                else {
+                    if(!col != _cols[i]) {
+                        return false;
+                    }
                 }
             }
+            return true;
         }
     public:
         graph() : _nodes{std::vector<node>()}, _cur_idx{0} {}
@@ -39,6 +48,7 @@ class graph {
             }
             _idx_map[name] = _cur_idx;
             _nodes.push_back({_cur_idx, std::vector<int>()});
+            _cols.push_back(false);
             _cur_idx++;
         }
         void add_edge(std::string& name1, std::string& name2) {
@@ -46,11 +56,10 @@ class graph {
             _nodes[i].adj.push_back(j);
             _nodes[j].adj.push_back(i);
         }
-        int n_connected_comp() {
+        bool is_bipartite() {
             // BFS on each component O(V+E)
             std::vector<bool> visited(_nodes.size());
             std::fill(visited.begin(), visited.end(), false);
-            int n_comp = 0;
             while(!std::all_of(visited.begin(), visited.end(), [](bool b){ return b; })) {
                 int idx;
                 // std::first_of + iterator -> index simpler?
@@ -59,10 +68,11 @@ class graph {
                         break;
                     }
                 }
-                bfs(visited, idx);
-                n_comp++;
+                // see if the next connected component is bipartite.
+                if(!bfs(visited, idx, false))
+                    return false;
             }
-            return n_comp;
+            return true;
         }
 };
 
@@ -82,8 +92,8 @@ graph read_input() {
 }
 
 std::string solve(graph& members) {
-    int n_comp = members.n_connected_comp();
-    return (n_comp == 1) ? "No" : "Yes";
+    bool is_bip = members.is_bipartite();
+    return is_bip ? "Yes" : "No";
 }
 
 int main() {
