6112eec91213ed3ca28432ba181fe2d8ce765853
Jack Wright <jack.louie.wright@gmail.com>
2022-06-18 21:12:30 +0100

    accidentally finding least weight path instead of greatest weight one

--- 2013/roundB/task3.py
+++ 2013/roundB/task3.py
@@ -5,46 +5,84 @@
 
 # of the shortest paths, choose the one with the maximum power.
 
-# need to get ALL shortest paths from entrance to exit, BFS and don't go one step deeper once a
-# path of length i is found.
+# expand out from en until reach ex, label cells 0 to NSTEP based on which step they were first
+# visited. As we want shortest number of steps, these cells' weights are the least weights obtained
+# at this stage
 
-import queue
+# 2x3
+# 0<=x<=N and 0<=y<=M (0,0) (0,1) (0,2) OR (0,0) (1,0) (2,0)  0<=x<=M and 0<=y<=N
+#                     (1,0) (1,1) (1,2) ?? (0,1) (1,1) (2,1)
 
-def get_nbrs(maze, M, N, px, py):
-    return None
+# need a custom __eq__ for in to work with a list of these objects
+class pt:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.y == other.y)
+    def __hash__(self):
+        return hash(repr(self))
+    def __repr__(self):
+        return "(" + str(self.x) + ", " +  str(self.y) + ")"
 
-def solve(maze, M, N, en_x, en_y, ex_x, ex_y):
+def get_nbrs(maze, M, N, p):
+    nbrs = []
+    if 0 <= p.x + 1 < N and maze[p.x+1][p.y] != -1:
+        nbrs.append(pt(p.x+1, p.y))
+    if 0 <= p.x - 1 < N and maze[p.x-1][p.y] != -1:
+        nbrs.append(pt(p.x-1, p.y))
+    if 0 <= p.y + 1 < M and maze[p.x][p.y+1] != -1:
+        nbrs.append(pt(p.x, p.y+1))
+    if 0 <= p.y - 1 < M and maze[p.x][p.y-1] != -1:
+        nbrs.append(pt(p.x, p.y-1))
+    return nbrs
+
+# we want the MAXIMUM weight path
+def solve(maze, M, N, en, ex):
     # do a BFS to get all shortest paths
-    pts = queue.Queue()
-    seen = [[False]*M]*N
-    seen[en_x][en_y] = True
-    pts.put([en_x, en_y, 0])
-    stop = False
-    # iterative BFS, how to record path?
-    while pts.qsize() != 0 and not stop:
-        pt_x, pt_y, lvl = pts.get()
-        nbrs = get_nbrs(maze, M, N, pt_x, pt_y)
-        for nbr_x, nbr_y in nbrs:
-            if not seen[nbr_x][nbr_y]:
-                seen[nbr_x][nbr_y] = True
-                pts.put([nbr_x, nbr_y, lvl+1])
-
-    return None
+    wghts = [[float("-inf")]*M for _ in range(N)] 
+    dist = [[None]*M for _ in range(N)]
+    dist[en.x][en.y], wghts[en.x][en.y] = 0, maze[en.x][en.y]
+    cur_dist = 0
+    pts = set()
+    pts.add(en)
+    # break out if we've got the endpoint or no new points.
+    while True:
+        new_pts = set()
+        for pt in pts:
+            nbrs = get_nbrs(maze, M, N, pt)
+            for p in nbrs:
+                # have we already visited this point? what if it gets visited multiple times, we still want multiple prevs
+                if dist[p.x][p.y] == None or dist[p.x][p.y] == cur_dist+1:
+                    new_pts.add(p)
+                    dist[p.x][p.y] = cur_dist+1
+                    w = wghts[pt.x][pt.y] + maze[p.x][p.y]
+                    if w > wghts[p.x][p.y]:
+                        wghts[p.x][p.y] = w
+        cur_dist += 1
+        pts = new_pts
+        # we found the exit
+        if ex in new_pts:
+            break
+        # we have failed to reach the exit
+        if new_pts == set():
+            return None
+    w = wghts[ex.x][ex.y]
+    return w
 
 def main():
     ncases = int(input())
-    # this may be necessary 
     for case in range(1, ncases+1):
         words = input().split()
         N, M = [int(word) for word in words]
         words = input().split()
         en_x, en_y, ex_x, ex_y = [int(word) for word in words]
+        en, ex = pt(en_x, en_y), pt(ex_x, ex_y)
         maze = []
         for _ in range(N):
             words = input().split()
             ints = [int(word) for word in words]
             maze.append(ints)
-        res = solve(maze, M, N, en_x, en_y, ex_x, ex_y)
+        res = solve(maze, M, N, en, ex)
         if res == None:
             print(f"Case #{case}: Mission Impossible.")
         else:
