c6e4682d0fcef7a3e8830ab9567011f666c0cab1
Jack Wright <jack.louie.wright@gmail.com>
2022-06-19 14:54:56 +0100

    task4 stuck, task5 solves sample but not either of the test cases

--- 2013/roundB/task4.py
+++ 2013/roundB/task4.py
@@ -1,6 +1,78 @@
+# another 2D grid. (N, W, E, S), dist (x1, y1) to (x2, y2) is |x1-x2| + |y1-y2|
+
+# rectangular areas (x1, y1, x2, y2), filled with integer points. Given several rectangles.
+
+# choose a selected point with minimal sum of all distances from other points to that point.
+# if multiple must be the one with the smallest x, if multiple smallest x, additionally one with
+# smallest y.
+
+# rectangles DO NOT INTERSECT
+
+# strategy: for every rectangle, we compress all other rectangles to a line/point
+# that can be moved up/down or left/right into the desired rectangle. Once all
+# other rectangles moved together, then calculate the best point in the "weighted"
+# rectangle by considering dx and dy seperately (weight is number of points moved to
+# that point from other rectangles.)
+
+class rect():
+    def __init__(self, x1, y1, x2, y2):
+        self.x1, self.x2, self.y1, self.y2 = x1, x2, y1, y2
+        self.wght = [[1]*(x2 - x1 + 1)]*(y2 - y1 + 1)
+    def __repr__(self):
+        s = f"(({self.x1}, {self.x2}), ({self.y1}, {self.y2}))"
+        return s
+    # assume oth is a rectangle inside self
+    def add(self, oth):
+        for x in range(oth.x1, oth.x2+1):
+            for y in range(oth.y1, oth.y2+1):
+                self.wght[x][y] += oth.wght[x][y]
+    def squash_into_x(self, x1, x2):
+        n_mov = 0
+        if self.x2 < x1:
+            self.x1 = self.x2 = x1
+        elif self.x1 > x2:
+            self.x1 = self.x2 = x2
+        elif self.x1 < x1 and self.x2 > x2:
+            self.x1, self.x2 = x1, x2
+        return n_mov
+    # move rect oth onto self.
+    def move_rect_to(self, oth):
+        w_oth = rect(oth.x1, oth.y1, oth.x2, oth.y2)
+        n_mov = 0
+        n_mov += w_oth.squash_into_x(self.x1, self.x2)
+        self.add(w_oth)
+        return n_mov
+    def find_optimum(self):
+        return 0, 0, 0
+
+def solve(rects):
+    # solution with minimal x, then minimal y
+    x, y, d = float("inf"), float("inf"), float("inf")
+    for rec in rects:
+        x_cur, y_cur, d_cur = None, None, None
+        wrect = rect(rec.x1, rec.y1, rec.x2, rec.y2)
+        for oth_r in rects:
+            if oth_r == rec:
+                continue
+            # distance from moving all points into this rectangle
+            d_cur += wrect.move_rect_to(oth_r)
+            # distance from moving all (weighted) points to the optimal point
+            x_cur, y_cur, D = wrect.find_optimum()
+            d_cur += D
+        if d_cur <= d and x_cur<=x and y_cur <= y:
+            x, y, d = x_cur, y_cur, d_cur
+    return 0, 0, 0
+
 def main():
     ncases = int(input())
     for case in range(1, ncases+1):
-        pass
+        B = int(input())
+        rects = []
+        for _ in range(B):
+            words = input().split()
+            x1, y1, x2, y2 = [int(word) for word in words]
+            rects.append(rect(x1, y1, x2, y2))
+        x, y, d = solve(rects)
+        print(f"Case #{case}: {x} {y} {d}")
 
 main()
--- 2013/roundB/task5.py
+++ 2013/roundB/task5.py
@@ -1,6 +1,131 @@
+# NxN hexagonal board, two sides R and B, board starts empty, both sides place colored stones
+# on any non-occupied square. Starting player is determined randomly (50-50)
+
+# top + bottom sides of board R, left + right sides B. Square 'R', 'B' or '.'
+# each interior square has 6 neighbours.
+
+# indexed S[row][col].
+
+# get given game state, tell whether it was valid and if it was who won.
+
+# possible invalid states:
+# - the number of Bs nB and number of As differ by more than 1.
+# - One side has won in two or more different ways.
+# - Both sides have "won"
+
+#   B B B
+# B B   B B - is still valid even though multiple paths, it's if there are multiple endpoints
+#   B B B   - joined to a single endpoint.
+
+# all hexagons indexed from top left (0, 0)
+
+# (0,0) (0,1)
+# (1,0) (1,1)
+
+class pt:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+# get neighbours of hexagon at point p, with color "R" if board is NxN
+def get_nbrs(squares, p, N, match="R"):
+    nbrs, ret_nbrs = [], []
+    if 0 < p.x - 1 < N:
+        nbrs += [pt(p.x-1, p.y)]
+    if 0 < p.x + 1 < N:
+        nbrs += [pt(p.x+1, p.y)]
+    if 0 < p.y - 1 < N:
+        nbrs += [pt(p.x, p.y-1)]
+    if 0 < p.y + 1 < N:
+        nbrs += [pt(p.x, p.y+1)]
+    # extra hexagonal neighbours (x+1, y-1), (x-1, y+1)
+    if 0 < p.x + 1 < N and 0 < p.y - 1 < N:
+        nbrs += [pt(p.x+1, p.y-1)]
+    if 0 < p.x - 1 < N and 0 < p.y + 1 < N:
+        nbrs += [pt(p.x-1, p.y+1)]
+    for n in nbrs:
+        if squares[n.x][n.y] == match:
+            ret_nbrs.append(n)
+    return ret_nbrs
+
+def is_dest(pt, N, hor=False):
+    if hor:
+        return pt.y == N-1
+    else:
+        return pt.x == N-1
+
+# get the number of ways a given side has won. If >1 we already know it's
+# impossible.
+def get_nwon(squares, N, starts, ends, hor=False):
+    side = "B" if hor else "R"
+    ways = 0
+    for start in starts:
+        stack = [start]
+        visited = []
+        while stack != []:
+            v = stack.pop()
+            # found a path
+            if v in ends:
+                ways += 1
+            if v not in visited:
+                visited.append(v)
+                nbrs = get_nbrs(squares, v, N, side)
+                for n in nbrs:
+                    stack.append(n)
+        if ways > 1:
+            return ways
+    return ways
+
+# returns number of ways blue has won wB, and number of ways red has won wR.
+def get_ways_won(squares, N):
+    wB, wR = 0, 0
+    # top to bottom paths winning for red
+    starts, ends = [], []
+    for y in range(N):
+        if squares[0][y] == "R":
+            starts.append(pt(0, y))
+        if squares[N-1][y] == "R":
+            ends.append(pt(N-1, y))
+    wR = get_nwon(squares, N, starts, ends, hor=False)
+    starts, ends = [], []
+    # left to right paths winning for blue
+    for x in range(N):
+        if squares[x][0] == "B":
+            starts.append(pt(x, 0))
+        if squares[x][N-1] == "B":
+            ends.append(pt(x, N-1))
+    wB = get_nwon(squares, N, starts, ends, hor=True)
+    return wR, wB
+
 def main():
     ncases = int(input())
     for case in range(1, ncases+1):
-        pass
-
+        N = int(input())
+        nred, nblue = 0, 0
+        squares = []
+        for i in range(N):
+            line = input()
+            squares.append(line)
+            for j in range(N):
+                if line[j] == "R":
+                    nred += 1
+                elif line[j] == "B":
+                    nblue += 1
+        # one side would have had to have moved twice in a row
+        if abs(nred - nblue) > 1:
+            print(f"Case {case}: Impossible")
+            continue
+        wR, wB = get_ways_won(squares, N)
+        if wB + wR > 1:
+            print(f"Case {case}: Impossible")
+        elif wB == 1:
+            print(f"Case {case}: Blue wins")
+        elif wR == 1:
+            print(f"Case {case}: Red wins")
+        else:
+            print(f"Case {case}: Nobody wins")
+            
 main()
