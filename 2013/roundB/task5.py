ae08db7e53c532412fc5cb5d3c41e36c3eb8985b
Jack Wright <jack.louie.wright@gmail.com>
2022-06-19 17:43:51 +0100

    see how to categorise an impossible board but difficult to implement

--- 2013/roundB/task5.py
+++ 2013/roundB/task5.py
@@ -9,13 +9,26 @@
 # get given game state, tell whether it was valid and if it was who won.
 
 # possible invalid states:
-# - the number of Bs nB and number of As differ by more than 1.
-# - One side has won in two or more different ways.
-# - Both sides have "won"
+# 1) - the number of Bs nB and number of As differ by more than 1.
+# 2) - both B and R have a winning path.
+# 3) - B/R's last move was winning but R/B has one more stone than B/R
+# 4) - Considering the graph of colored R/B paths from one side of the board to the other, this graph does not
+#      have an articulation vertex (so the side would have won on the last move and kept playing.) 
 
 #   B B B
 # B B   B B - is still valid even though multiple paths, it's if there are multiple endpoints
 #   B B B   - joined to a single endpoint.
+
+# B B
+#   B B - is also a valid winning configuration, blue could have started from the LHS.
+# B B
+
+#   B B
+# B B   - ok as could have started from RHS
+#   B B
+
+# connected components of Bs? containing at least one start/end point. If a connected component contains 2 start pts 
+# and 2 end pts we know the board configuration is impossible.
 
 # all hexagons indexed from top left (0, 0)
 
@@ -51,14 +64,7 @@ def get_nbrs(squares, p, N, match="R"):
             ret_nbrs.append(n)
     return ret_nbrs
 
-def is_dest(pt, N, hor=False):
-    if hor:
-        return pt.y == N-1
-    else:
-        return pt.x == N-1
-
-# get the number of ways a given side has won. If >1 we already know it's
-# impossible.
+# we are in an impossible configuration if:
 def get_nwon(squares, N, starts, ends, hor=False):
     side = "B" if hor else "R"
     ways = 0
@@ -114,7 +120,7 @@ def main():
                     nred += 1
                 elif line[j] == "B":
                     nblue += 1
-        # one side would have had to have moved twice in a row
+        # Condition 1) for impossibility.
         if abs(nred - nblue) > 1:
             print(f"Case {case}: Impossible")
             continue
